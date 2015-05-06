96b80abddd9697c92422f44c4c9ba7aecb2eec60
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2015-05-06 11:09:50 -0300

    simplify the code in controllers / mailers / models

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,13 +12,14 @@ class SessionsController < ApplicationController
   # Create sessions when users perform login 
   def create
     user = User.authenticate(params[:username], params[:password])
+    user_id = user.id
     # Check the User existence and if your account is active
     if user && user.account_status == true
-      session[:remember_token] = user.id
-      session[:user_id] = user.id
-      redirect_to root_path, :notice => "Bem vindo ao TEM-DF! :D"
+      session[:remember_token] = user_id
+      session[:user_id] = user_id
+      redirect_to root_path, :notice => "Seja bem vindo!"
     else
-      flash.now.alert = "Usuário ou senha inválidos"
+      flash.now.alert = "Usuário ou senha inválidos!"
       render "new"
     end
   end
@@ -27,6 +28,6 @@ class SessionsController < ApplicationController
   def destroy
     session[:remember_token] = nil
     session[:user_id] = nil
-    redirect_to root_path, :notice => "Tchau! Volte sempre! :)"
+    redirect_to root_path, :notice => "Obrigado, volte sempre!"
   end
 end
--- app/mailers/temdf_mailer.rb
+++ app/mailers/temdf_mailer.rb
@@ -7,30 +7,32 @@
 class TemdfMailer < ActionMailer::Base
   
   default from: "temdf.unb@gmail.com"
+  default to: "temdf.unb@gmail.com"
 
   # Email template for medical registries in the application
   def request_account
     path_date_medical_file = "public/uploads/arquivo_medico"
     attachments["arquivo_medico.pdf"] = File.read(path_date_medical_file)
-  	mail(to: "temdf.unb@gmail.com", 
-         subject: "Nova solicitacao de medico", 
+  	mail(subject: "Nova solicitacao de medico", 
          body: "solicitacao de cadastro de usuario")
   end
 
   # Email template for contact between user and administrator
   def send_mail (subject, message, email, user_name)
-   	mail(to: "temdf.unb@gmail.com", 
-         subject: subject,
+   	mail(subject: subject,
 		     body: "From: " + email + "\nNome: " + user_name + "\n\n" + message)
   end
   
   # Email template for registration confirmation
   def confimation_email (user_id, user_token_email, email)
-    link = "http://0.0.0.0:3000/confirmation/" + user_id.to_s() + "/" + user_token_email.to_s()
+    address_confirmation_page = "http://0.0.0.0:3000/confirmation/"
+    link_confirm_registration = address_confirmation_page + user_id.to_s() + "/" + user_token_email.to_s()
 
     mail(to: email,
          subject: "Confirme seu cadastro em TEM-DF!",
-         body: "Confirme seu cadastro no link abaixo:\n\n#{link}")
+         body: "Obrigado por se cadastrar!\n   
+                Confirme seu cadastro clicando no link abaixo:\n\n
+                #{link_confirm_registration}")
   end  
 end
 
